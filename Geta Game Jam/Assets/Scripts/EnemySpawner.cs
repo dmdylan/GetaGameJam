547c7684991f083b2026e813e0ef37487c877707
dmdylan <dylandanowski@gmail.com>
2020-01-04 11:00:49 -0500

    Fixed enemy spawning mechanics

--- Geta Game Jam/Assets/Scripts/EnemySpawner.cs
+++ Geta Game Jam/Assets/Scripts/EnemySpawner.cs
@@ -4,55 +4,67 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private Transform[] enemySpawners;
+    [SerializeField] private Transform[] enemySpawners = null;
     public FloatValue enemyCount;
-    public Enemy[] enemyPrefab;
+    public Enemy[] enemyPrefab = null;
 
 
     // Start is called before the first frame update
     void Start()
-    {
-        AddEnemiesToList();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+    { 
         
+        SpawnEnemies();
+
     }
 
+    //Create list of open spawn points and a list of enemies from other method
+    //Run through list of enemies and spawn them at random open location
     void SpawnEnemies()
     {
         List<Transform> freeSpawnPoints = new List<Transform>(enemySpawners);
-        
-       
+        List<Enemy> enemies = AddEnemiesToList();
 
-
+        for(int i = 0; i < enemies.Count; i++)
+        {
             int spawnPointIndex = Random.Range(0, enemySpawners.Length);
             Transform position = freeSpawnPoints[spawnPointIndex];
-            //use for loop to loop through each enemy in separate enemy list
-       
-        return;
-    }
-
-    private List<Enemy> AddEnemiesToList()
-    {
-        var currentEnemyCount = 0f; 
-        List<Enemy> enemiesToSpawn = new List<Enemy>();
-
-        for(int i = 0; i < enemyCount.Value;)
-        {
-            var enemy = enemyPrefab[Random.Range(0, enemyPrefab.Length)];
-            enemiesToSpawn.Add(enemy);
-            currentEnemyCount += enemy.profile.enemyValue;
-            foreach (Enemy x in enemiesToSpawn)
-                print(x);
+            freeSpawnPoints.RemoveAt(spawnPointIndex);
+            Instantiate(enemies[i], position.position, position.rotation);           
         }
-
-        return enemiesToSpawn;
+        
+        return;
     }
 
     //Separate method that runs through random enemies to get count
     //Adds those enemies to a list
     //Then call that method and instantiate enemies
+    private List<Enemy> AddEnemiesToList()
+    {
+        List<Enemy> enemiesToSpawn = new List<Enemy>();
+        var currentEnemyCountValue = 0f;
+
+        for(int i = 0; i < enemyCount.Value;)
+        {
+            var enemy = enemyPrefab[Random.Range(0, enemyPrefab.Length)];
+            if(enemy.profile.enemyValue + i > 100)
+            {
+                enemy = enemyPrefab[Random.Range(0, enemyPrefab.Length)];
+            }
+            else
+            {
+                i += enemy.profile.enemyValue;
+                currentEnemyCountValue = i;
+                enemiesToSpawn.Add(enemy);
+            }
+        }
+
+        foreach (Enemy x in enemiesToSpawn)
+        {
+            print(x);
+            print(currentEnemyCountValue);
+        }   
+
+        return enemiesToSpawn;
+    }
+
 }
